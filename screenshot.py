b54d2d293428403fc4b22b14da8a92f2e3317abc
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-22 00:09:37 +0100

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,4 @@
-# Installed PyAutoGUI and Pillow also
+# Installed PyAutoGUI and Pillow packages 
 
 import time
 import pyautogui
@@ -16,13 +16,15 @@ def screenshot():
 screenshot()
 
 #The above overwrites previous screenshots that are made each time the program is run
+
 #To avoid this and save each with its own different names, we can modify the function as below
 
 def screenshot():
     # we can replace below using the random.randint() to generate a random number and store it in the 'name' variable
     # that then will also mean we will need to import random module
     name = int(round(time.time() * 1000))
-    name = f"{name}.png"
+    # we also created a new sub-folder to store all screenshots using its abolute path
+    name = f"C:/Users/nwoko/PycharmProjects/myfirstproject/screenshots/{name}.png"
     time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
