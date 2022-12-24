900f8bf1f7212e465066ec8248b902b4e9792fb6
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-24 14:18:34 +0100

    Create webcam_app.py

---
+++ webcam_app.py
@@ -0,0 +1,14 @@
+# install opencv-python package
+
+import cv2
+
+imgcapture = cv2.VideoCapture(0)
+result = True
+
+while(result):
+    ret, frame = imgcapture.read()
+    cv2.imwrite("test.jpg", frame)
+    result = False
+    print("Image Captured...")
+
+imgcapture.release()
